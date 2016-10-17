b3eb8aac00131051c6728ab9569873e13168add5
Martin C. Richards <martin@travelstart.co.za>
2016-10-17 08:49:38 +0200

    init commit

---
+++ src/main/java/me/martinrichards/apache/camel/Main.java
@@ -0,0 +1,48 @@
+package me.martinrichards.apache.camel;
+
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.guice.CamelModuleWithRouteTypes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by martinrichards on 2016/10/16.
+ */
+public class Main {
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+    private final CamelContext context;
+
+    @Inject
+    public Main(CamelContext camelContext){
+        context = camelContext;
+    }
+
+    public static void main(String... args) throws Exception {
+        Injector injector = Guice.createInjector(new CamelModuleWithRouteTypes());
+        final Main app = injector.getInstance(Main.class);
+        app.start();
+        log.info("Application has started!");
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run()  {
+                try {
+                    app.stop();
+                } catch (Exception ex) {
+                    log.error("Error stopping", ex);
+                }
+            }
+        });
+    }
+
+    public void start() throws Exception {
+        context.start();
+    }
+
+    public void stop() throws Exception {
+        context.stop();
+    }
+}
---
+++ src/test/java/me/martinrichards/apache/camel/MainTest.java
@@ -0,0 +1,15 @@
+package me.martinrichards.apache.camel;
+
+import org.junit.Test;
+
+/**
+ * Created by martinrichards on 2016/10/16.
+ */
+public class MainTest {
+
+    @Test
+    public void testMainRuns() {
+//        Main sub = new Main();
+//        sub.run();
+    }
+}
